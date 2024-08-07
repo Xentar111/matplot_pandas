c8eae6e4cd8e69c3af4fdb4683a9de9696a6e19f
[Carlos Matias] <xentar111@gmail.com>
2024-08-07 18:37:20 -0300

    arreglo del pd.categorical, falta asignar vlaor unico a cada categoria

--- main.py
+++ main.py
@@ -9,45 +9,24 @@ import os
 annual_enterprise = pd.read_csv('input/annual-enterprise-survey-2023-financial-year-provisional.csv')
 
 print(annual_enterprise.head())
-#print(annual_enterprise.shape)
 
-#print(annual_enterprise.columns)
-#print(annual_enterprise['Variable_code'])
-#print(annual_enterprise.dtypes)
+annual_enterprise['Units'] = pd.Categorical(annual_enterprise['Units'])#series... no necesita conversion
+annual_enterprise['Variable_code'] = pd.Categorical(annual_enterprise['Variable_code'])
 
 
-series_units = pd.Series(pd.Categorical(annual_enterprise['Units']))
-#print(type(series_units))
-print(series_units)
+# Crear valores aleatorios para colores y áreas
+N = len(annual_enterprise)
+colors = np.random.rand(N)
+area = (30 * np.random.rand(N)) ** 2
 
-plt.scatter(annual_enterprise['Units'], annual_enterprise['Variable_code'], color='red')
-for i in range(series_units):
-    plt.annotate(f"({annual_enterprise['Units'][i]}, {annual_enterprise['Variable_code'][i]}), (annual_enterprise['Units'][i]), annual_enterprise['Variable_code'][i]")
+# Crear el gráfico de dispersión
+plt.figure(figsize=(10, 6))
+scatter = plt.scatter( s=area, c=colors, alpha=0.5, marker='v')
 
-plt.legend()
+# Añadir etiquetas y título
+plt.xlabel('Units')
+plt.ylabel('Variable Code')
+plt.title('Scatter Plot de Annual Enterprise Survey')
+
+# Mostrar la gráfica
 plt.show()
-
-fig, ax = plt.subplots()
-#ax.plot(annual_enterprise['Industry_aggregation_NZSIOC'])
-
-#Data
-ax.scatter(y=annual_enterprise['Industry_name_NZSIOC'], x=annual_enterprise['Industry_aggregation_NZSIOC'])
-
-#GUI
-ax.set_xlabel('Industry_NZSIOC')
-ax.set_ylabel('Industry_name_NZSIOC')
-ax.set_title('Enterprise_survey_2023')
-
-# Units provisional??
-fig, provisional_ = plt.subplots()
-
-#
-#print(annual_enterprise['Units'])
-#print(annual_enterprise['Value'])
-
-#provisional_.scatter(annual_enterprise['Units'], annual_enterprise['Value'])#mal implementado
-#provisional_.scatter(x=annual_enterprise['Units'], y=annual_enterprise['Value'])
-
-# A;adir division de industry_code, variable_categoria, 
-# variable_name, tambien year esas creo q son las mas importantes
-# plt.show()
