41eb397d8bbeb902ebf2202526a5ef93392801c1
[Carlos Matias] <xentar111@gmail.com>
2024-08-08 05:32:37 -0300

    food price analisis

--- food_price_index_september_analisis.py
+++ food_price_index_september_analisis.py
@@ -4,6 +4,16 @@ import pandas as pd
 food_price = pd.read_csv('input/food-price-index-september-2023-weighted-average-prices.csv')
 
 print(food_price.head())
+#print(len(set(food_price['Series_title_1'].values)))
+#print(len(set(food_price['Series_reference'].values)))
+print(len(food_price['Data_value'].values))
 
-plt.plot(food_price['Data_value'], food_price['Period'])
-plt.show()
+
+filter_df = food_price[food_price['Series_reference'] == 'CPIM.SAP0100']
+
+print(filter_df.head())
+print(filter_df.shape)
+
+print(pd.to_datetime(filter_df['Period']))
+
+#filter_df['Period']
