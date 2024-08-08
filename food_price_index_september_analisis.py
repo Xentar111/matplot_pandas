61ad6d095917718c906df3a6f852bf3dfb6922d9
[Carlos Matias] <xentar111@gmail.com>
2024-08-08 05:43:56 -0300

    resolver despues

--- food_price_index_september_analisis.py
+++ food_price_index_september_analisis.py
@@ -14,6 +14,16 @@ filter_df = food_price[food_price['Series_reference'] == 'CPIM.SAP0100']
 print(filter_df.head())
 print(filter_df.shape)
 
-print(pd.to_datetime(filter_df['Period']))
+print(pd.to_datetime(filter_df['Period'], format='%Y.%m'))
+""" 
+ValueError: time data "2006" doesn't match format "%Y.%m", at position 0. You might want to try:
+    - passing `format` if your strings have a consistent format;
+    - passing `format='ISO8601'` if your strings are all ISO8601 but not necessarily in exactly the same format;
+    - passing `format='mixed'`, and the format will be inferred for each element individually. You might want to use `dayfirst` alongside this.
+"""
+
 
 #filter_df['Period']
+
+filter_df = filter_df.sort_values(by='Period')
+
