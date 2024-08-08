aab9a036bcd2a8ea2cb55243349102c046b5c813
[Carlos Matias] <xentar111@gmail.com>
2024-08-08 18:58:15 -0300

    aun asi no se resuleve linea 19

--- food_price_index_september_analisis.py
+++ food_price_index_september_analisis.py
@@ -12,10 +12,15 @@ print(len(food_price['Data_value'].values))
 
 filter_df = food_price[food_price['Series_reference'] == 'CPIM.SAP0100']
 
-print(filter_df.head())
-print(filter_df.shape)
+#print(filter_df.head())
+#print(filter_df.shape)
 
-#print(pd.to_datetime(filter_df['Period'], format='%Y.%m'))
+
+def parse_period(period):
+    try:
+        print(pd.to_datetime(period, format='%Y.%m'))
+    except ValueError:
+        print(pd.to_datetime(period, format='%Y.%m', errors='corece'))
 """ 
 ValueError: time data "2006" doesn't match format "%Y.%m", at position 0. You might want to try:
     - passing `format` if your strings have a consistent format;
@@ -23,9 +28,9 @@ ValueError: time data "2006" doesn't match format "%Y.%m", at position 0. You mi
     - passing `format='mixed'`, and the format will be inferred for each element individually. You might want to use `dayfirst` alongside this.
 """
 
+filter_df['Period'] = filter_df['Period'].apply(parse_period)
 
-#filter_df['Period']
-
+filter_df = filter_df.dropna(subset=['Period'])
 filter_df = filter_df.sort_values(by='Period')
 
 print(filter_df.head())
@@ -40,4 +45,6 @@ plt.hist(filter_df['Data_value'])#, bins=np.unique(filter_df['Period']))
 print(filter_df.tail())
 plt.xlabel('Data Value')
 plt.ylabel('Period')
+plt.title('Orange Values')
+plt.grid(True)
 plt.show()
